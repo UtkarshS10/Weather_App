2a258c1e92663709225c4c5fe1e8d3bd7c662b24
UtkarshS10 <utkarsh.25bce10896@vitbhopal.ac.in>
2026-03-26 00:08:28 +0530

    Added location tracker for easy and efficient use . Worked on error handling.

--- script.js
+++ script.js
@@ -9,30 +9,69 @@ const cityName=document.getElementById("city-name");
 const cityTime=document.getElementById("city-time");                                    
 const cityTemp=document.getElementById("city-temp");      
 const cityHumidity=document.getElementById("city-humidity");                                 const cityCondition=document.getElementById("city-condition"); 
+const getlocation=document.getElementById("get-location");
+const button1=document.getElementById("location");
+const message=document.getElementById("weather-message"); 
 const sunny = "url('https://images.unsplash.com/photo-1501973801540-537f08ccae7b')";
 const cloudy = "url('https://images.unsplash.com/photo-1501630834273-4b5604d2ee31')";
 const rain = "url('https://images.unsplash.com/photo-1501691223387-dd0500403074')";
 const snow = "url('https://images.unsplash.com/photo-1608889175123-8ee362201f81')";                                                                                       
-async function getData(cityName) { 
+async function getData1(cityName) { 
     const data=await fetch (`https://api.weatherapi.com/v1/current.json?key=61d39bdfda7e4721a4c63619262003&q=${cityName}&aqi=yes`) 
     if(!data.ok) {
-        throw new error("Failed to fetch data, try again") 
+        // throw new error("Failed to fetch data, try again") 
+        message.innerText="Error! Enter Correct Place.";
+        message.className="mt-4 text-center font-semibold text-lg text-red-500";
+        return null;
     }
      return await data.json() 
-}
-button.addEventListener("click", async ()=> {
-    const value=input.value
-    const result=await getData(value) 
-    cityName.innerText=`${result.location.name},${result.location.region} - ${result.location.country}`
-    cityTime.innerText=result.location.localtime
-    cityTemp.innerText=result.current.temp_c
-    cityHumidity.innerText=result.current.humidity+"%" 
-    cityCondition.innerText=result.current.condition.text 
+} 
 
-const body = document.getElementById("app-body");
-const message=document.getElementById("weather-message");
-const condition = result.current.condition.text.toLowerCase();
-const temp=result.current.temp_c;
+async function getData(lat, long) { 
+    const data=await fetch (`https://api.weatherapi.com/v1/current.json?key=61d39bdfda7e4721a4c63619262003&q=${lat},${long}&aqi=yes`) 
+    if(!data.ok) {
+    message.innerText="Error! Try Again.";
+    message.className="mt-4 text-center font-semibold text-lg text-red-500";
+    return null;
+    }
+    return await data.json() 
+} 
+async function gotLocation(position) {
+    const result=await getData(position.coords.latitude, position.coords.longitude);
+    console.log(result);
+    updateUI(result);
+} 
+
+function failedToGet() {
+    console.log("There was some issue") 
+     
+} 
+
+async function handleClick(e) {
+    
+    if (e.target.id === "location") {
+        navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
+        
+    } 
+
+    else {
+        const value = input.value;
+        const result = await getData1(value);
+        if(!result) return;
+        updateUI(result);
+    }} 
+
+function updateUI(result) {
+        cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
+        cityTime.innerText = result.location.localtime;
+        cityTemp.innerText = result.current.temp_c;
+        cityHumidity.innerText = result.current.humidity + "%";
+        cityCondition.innerText = result.current.condition.text;
+
+    const body = document.getElementById("app-body");
+    const message=document.getElementById("weather-message");
+    const condition = result.current.condition.text.toLowerCase();
+    const temp=result.current.temp_c;
     body.style.backgroundSize = "cover";
     body.style.backgroundPosition = "center";
     body.style.backgroundRepeat = "no-repeat";
@@ -62,7 +101,12 @@ const temp=result.current.temp_c;
 }
 else {
   body.style.backgroundImage = "url('https://images.unsplash.com/photo-1506744038136-46273834b3fb')";
+  message.innerText="Enjoy your day!";
+  message.className="mt-4 text-center font-semibold text-lg text-indigo-400";
 }
-}) 
+} 
+    
+button1.addEventListener("click", handleClick);
+button.addEventListener("click", handleClick); 
 
 
